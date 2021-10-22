869dd631e7109de0cda0bcafbe2ddc766d15a0c5
Christian Hur <chrisvhur@gmail.com>
2021-10-22 10:19:00 -0500

    Update main.py

--- unit7/apply/main.py
+++ unit7/apply/main.py
@@ -1,18 +1,45 @@
-"""
+'''
+MIT License
+
+Copyright (c) 2021 Christian Hur (Gateway Technical College)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+------------
+Description:
+------------
+
 Write a program that prompts the user to enter information for vehicles, stocks, or people.  Save the data of each type to their own text file.  Finally, prompt the user to select a file to print.
 Ask the user to choose one of the following options for input:
-A = Auto
-S = Stocks
-P = Person
+    
+    A = Auto
+    S = Stocks
+    P = Person
 
-For each type, as for the following:
+For each type, ask the following:
 
-Auto:   Make, Model, Year, Price
-Stocks:  Company name, Symbol, Stock Price
-Person:  Name, Position, Salary
+    Auto:    Make, Model, Year, Price
+    Stocks:  Company name, Symbol, Stock Price
+    Person:  Name, Position, Salary
 
 Keep asking the user until they're done entering data.
-"""
+'''
 
 from auto_stock_person import main
 
