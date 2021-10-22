749bcfe3c93b5ebbf300cd1b6e2def440624600a
Christian Hur <chrisvhur@gmail.com>
2021-10-22 10:17:38 -0500

    Add files via upload

---
+++ unit7/apply/main.py
@@ -0,0 +1,20 @@
+"""
+Write a program that prompts the user to enter information for vehicles, stocks, or people.  Save the data of each type to their own text file.  Finally, prompt the user to select a file to print.
+Ask the user to choose one of the following options for input:
+A = Auto
+S = Stocks
+P = Person
+
+For each type, as for the following:
+
+Auto:   Make, Model, Year, Price
+Stocks:  Company name, Symbol, Stock Price
+Person:  Name, Position, Salary
+
+Keep asking the user until they're done entering data.
+"""
+
+from auto_stock_person import main
+
+main()
+
