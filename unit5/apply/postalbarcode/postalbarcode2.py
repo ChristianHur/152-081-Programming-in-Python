38aa02fa261e2c9e6fb0ffec7d039137214e4561
Christian Hur <chrisvhur@gmail.com>
2021-11-22 12:59:54 -0600

    Update postalbarcode2.py

--- unit5/apply/postalbarcode/postalbarcode2.py
+++ unit5/apply/postalbarcode/postalbarcode2.py
@@ -1,19 +1,31 @@
-"""
-Unit 5 - Assignment
-@author:  Christian Hur
-
-Create a Python program that asks the user for a 5-digit zip code and prints the bar code.
+'''
+MIT License
+Copyright (c) 2021 Christian Hur (Gateway Technical College)
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 -------------------------
-Background Information
+Description
 -------------------------
 For faster sorting of letters, the United States Postal Service encourages companies that send large volumes of mail to use a bar code denoting the zip code.
-
 The encoding scheme for a five-digit zip code is shown in Figure 13. There are full-height frame bars on each side. The five encoded digits are followed by a check digit, which is computed as follows: Add up all digits and choose the check digit to make the sum a multiple of 10. For example, the zip code 95014 has a sum of 19, so the check digit is 1 to make the sum equal to 20.
-
-Write a program that asks the user for a zip code and prints the bar code. Use : for half bars, | for full bars. For example, 95014 becomes:
+Write a program that asks the user for a zip code and prints the bar code. Use : for half bars, | for full bars. For example, 95014 becomes:
     
     ||:|:::|:|:||::::::||:|::|:::|||
-"""
+'''
+
 ##
 # Global constants
 #
