6ea3157a4d6ecd307b1ddbb072e667c5ca50bdf8
Christian Hur <chrisvhur@gmail.com>
2021-11-23 11:46:05 -0600

    Update main.py

--- unit7/practice/main.py
+++ unit7/practice/main.py
@@ -25,7 +25,9 @@ SOFTWARE.
 Description:
 ------------
 
-Write a program that reads a file containing text. Read each line and send it to the output file, in reverse order. If the input file contains:
+Write a program that reads a file containing text. Read each line and send
+it to the output file, in reverse order. If the input file contains:
+
 Mary had a little lamb
 Whose fleece was white as snow. 
 And everywhere that Mary went,
@@ -38,7 +40,10 @@ And everywhere that Mary went,
 Whose fleece was white as snow.
 Mary had a little lamb
 
-Also, print the number of characters (exclude white spaces), words, and lines in the input file.  We will consider a word is one or more characters surrounded by white spaces.  For example, the sample input file above should print:
+Also, print the number of characters (exclude white spaces), words, and lines
+in the input file.  We will consider a word is one or more characters surrounded
+by white spaces.  For example, the sample input file above should print:
+
 Character count: 89 
 Word count:  22
 Line count: 4
