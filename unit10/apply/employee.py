009e826a45cffe503755604f0de3f060710fb1ef
Christian Hur <chrisvhur@gmail.com>
2021-11-22 22:11:34 -0600

    Update employee.py

--- unit10/apply/employee.py
+++ unit10/apply/employee.py
@@ -1,3 +1,23 @@
 # Super class
 class Employee:
-  pass
+    def __init__(self,name,position,department,employeeType,weeklyPay):
+        self._name = name
+        self._position = position
+        self._department = department
+        self._weeklyPay = weeklyPay
+        self._type = employeeType
+        
+    def setName(self,name): self._name = name
+    def setPosition(self,position): self._position = position
+    def setDepartment(self,department): self._department = department
+    def setWeeklyPay(self,weeklyPlay): self._weeklyPay = weeklyPlay
+        
+    def getName(self): return self._name
+    def getPosition(self): return self._position
+    def getDepartment(self): return self._department
+    def getWeeklyPay(self): return self._weeklyPay
+    def getType(self): return self._type
+    
+    def __repr__(self):
+       return str([self._name,self._position,self._department])
+      
