f6ef87b32ba76785ab6d5bc9ca90aa261b21b9e7
Christian Hur <chrisvhur@gmail.com>
2021-11-22 22:12:34 -0600

    Update person.py

--- unit10/practice/person.py
+++ unit10/practice/person.py
@@ -1,2 +1,25 @@
 class Person:
-  pass
+  
+    def __init__(self,first,last,year_of_birth):
+        self._first = first
+        self._last = last
+        self._year_of_birth = year_of_birth
+        
+    def setFirst(self,first):
+        self._first = first
+        
+    def setLast(self,last):
+        self._last = last
+        
+    def setYearOfBirth(self,year_of_birth):
+        self._year_of_birth = year_of_birth
+        
+    def getFirst(self):
+        return self._first
+      
+    def getLast(self):
+        return self._last
+      
+    def getYearOfBirth(self):
+        return self._year_of_birth
+      
