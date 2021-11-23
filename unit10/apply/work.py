95c51429ed5b93482ded443d9f5828a57ec8ae0c
Christian Hur <chrisvhur@gmail.com>
2021-11-22 22:09:39 -0600

    Update work.py

--- unit10/apply/work.py
+++ unit10/apply/work.py
@@ -2,12 +2,42 @@ from regular_employee import RegularEmployee
 from supervisor import Supervisor
 
 def getEmployeeData(employeeType):
-  #TODO
-  pass
+    name = input('Employee name: ')
+    position = input('Position: ')
+    department = input('Department: ')
+    if employeeType == 'E':
+        hoursWorked = float(input('Hours Worked: '))
+        hourlyRate = float(input('Hourly Rate: '))
+        employee = RegularEmployee(name, position, department, employeeType, hoursWorked,hourlyRate)
+    else:
+        salary = float(input('Salary: '))
+        employee = Supervisor(name, position, department, employeeType, salary)
+    return employee
 
 def printWeeklyPay(employee):
-  #TODO
-  pass
+    
+    # Format currency to two decimal places
+    if employee.getType() == 'E':
+        overTimePay = '{:,.2f}'.format(employee.getOverTimePay())
+        regularPay = '{:,.2f}'.format(employee.getRegularPay())
+    else:
+        overTimePay = '{:,.2f}'.format(0)
+        regularPay = '{:,.2f}'.format(0)
+        
+    weeklyPay = '{:,.2f}'.format(employee.getWeeklyPay())
+    
+    # Print to screen
+    print()
+    print('*'*20)
+    print('EMPLOYEE WEEKLY PAY')
+    print('*'*20)
+    print(f'Name: {employee.getName()}')
+    print(f'Position: {employee.getPosition()}')
+    print(f'Department: {employee.getDepartment()}')
+    if employee.getType() == 'E':
+        print(f'Regular Pay: ${regularPay}')
+    print(f'Weekly Pay: ${weeklyPay}')
+    print(f'OverTime Pay: ${overTimePay}')
 
 def doMore():
     while True:
@@ -15,10 +45,22 @@ def doMore():
         if more not in 'YN':
             print('*** Invalid input.  Try again. ***') 
         else:
-            return more == 'N'   
-
+            return more == 'N'            
+                
 def main():
-  #TODO
-  pass
-
+    
+    done = False
+    while not done:
+        employee = None
+        while True:
+            employeeType = input('Employee or Supervisor [E or S]: ').upper()
+            if employeeType in 'ES':                
+                employee = getEmployeeData(employeeType)
+                break
+            else:
+                print('*** Invalid input.  Try again. ***')
+    
+        printWeeklyPay(employee)
+        done = doMore()    
+    
 main()
