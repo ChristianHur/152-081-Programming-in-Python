819efba130a2d211645cab69102c91ccbcaa89d2
Christian Hur <chrisvhur@gmail.com>
2021-11-22 22:11:11 -0600

    Update regular_employee.py

--- unit10/apply/regular_employee.py
+++ unit10/apply/regular_employee.py
@@ -2,4 +2,29 @@ from employee import Employee
 
 # Subclass
 class RegularEmployee(Employee):
-  pass
+    def __init__(self,name,position,department,employeeType,hoursWorked,hourlyRate):
+        super().__init__(name, position, department, employeeType, 0)
+        self._hoursWorked = hoursWorked
+        self._hourlyRate = hourlyRate
+        self._regularPay = self.calculateRegularPay()
+        self._overTimePay = self.calculateOverTimePay()        
+        self.calculatePay()
+        
+    def calculatePay(self):
+        self._weeklyPay = round(self._regularPay + self._overTimePay, 2)
+        
+    def calculateRegularPay(self):
+        if self._hoursWorked >= 40:
+            return round(40 * self._hourlyRate, 2)
+        return round(self._hoursWorked * self._hourlyRate,2)
+    
+    def calculateOverTimePay(self):
+        if self._hoursWorked > 40:
+            return round(self._hourlyRate * (self._hoursWorked - 40) * 1.5, 2)
+        return 0
+
+    def getOverTimePay(self):
+        return self._overTimePay
+    
+    def getRegularPay(self):
+        return self._regularPay
