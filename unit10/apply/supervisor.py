cfe077f5aff32d1402a3b947108aa6fa8fdca9b0
Christian Hur <chrisvhur@gmail.com>
2021-11-22 22:10:29 -0600

    Update supervisor.py

--- unit10/apply/supervisor.py
+++ unit10/apply/supervisor.py
@@ -2,4 +2,11 @@ from employee import Employee
 
 # Subclass
 class Supervisor(Employee):
-  pass
+  def __init__(self,name,position,department,employeeType,salary):
+        super().__init__(name, position, department, employeeType, 0)
+        self._salary = salary
+        
+        self.calculatePay()
+        
+    def calculatePay(self):
+        self._weeklyPay = round(self._salary / 52, 2)
