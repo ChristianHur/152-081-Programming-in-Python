1ee01042cd42f85670c4be6ad5f6b638b46c297a
Christian Hur <chrisvhur@gmail.com>
2021-11-23 11:25:51 -0600

    Create decimaltobinary.py

---
+++ unit4/practice/decimaltobinary.py
@@ -0,0 +1,21 @@
+"""
+Unit 4 - Practice (#2)
+@author:  Christian Hur
+
+Write a Python program that reads an integer value from the console and converts it into its binary equivalent . For example, if the user provides the input 13, the output should be:  1101. 
+"""
+
+#Variable
+binary = ""
+
+#Input
+number = int(input("Enter a positive integer:"))
+temp = number  #preserve original number for printing
+
+#Process
+while temp != 0:
+    binary = str(temp % 2) + binary
+    temp = temp // 2
+
+#Output
+print("The binary of {} is {}".format(number,binary))
