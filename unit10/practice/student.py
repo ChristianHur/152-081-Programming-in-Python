34b06cc09893c2a363e1b4523092aa96e8a9c70d
Christian Hur <chrisvhur@gmail.com>
2021-11-22 22:12:04 -0600

    Update student.py

--- unit10/practice/student.py
+++ unit10/practice/student.py
@@ -1,4 +1,18 @@
 from person import Person
 
 class Student(Person):
-  pass
+    def __init__(self,first,last,year_of_birth,major,gpa):
+        super().__init__(first,last,year_of_birth)
+        self._major = major
+        self._gpa = gpa
+        
+    def setMajor(self,major):
+        self._major= major
+        
+    def setGpa(self,gpa):
+        self._gpa = gpa
+    
+    def getMajor(self):
+        return self._major
+    def getGpa(self):
+        return self._gpa
