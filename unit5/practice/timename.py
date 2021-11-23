15dd24fe7a0b7e36453c7e3b2c1b6f70ec1e2328
Christian Hur <chrisvhur@gmail.com>
2021-11-23 11:41:38 -0600

    Update timename.py

--- unit5/practice/timename.py
+++ unit5/practice/timename.py
@@ -25,15 +25,19 @@ SOFTWARE.
 Description:
 ------------
 
-Write a function getTimeName(hours, minutes, period) that returns the English name for a point in time.  If the minutes are exactly 15, 30, or 45, the it should read "a quarter after ... or a quarter to ..., half past ...".  If the minutes is 0 then it should read "... o'clock".  Assume that hours is between 1 and 12.
+Write a function get_time_name(hours, minutes, period) that returns the English
+name for a point in time.  If the minutes are exactly 15, 30, or 45, the it
+should read "a quarter after ... or a quarter to ..., half past ...".  If the
+minutes is 0 then it should read "... o'clock".  Assume that hours is
+between 1 and 12.
 
 Examples:
 
-getTimeName(8,0,AM)                 # 8 o'clock am
-getTimeName(8,15,PM)               # a quarter after 8 pm
-getTimeName(8,30,AM)               # half past 8 am
-getTimeName(8,45,PM)               # a quater to 9 am
-getTimeName(8,12,AM)               # twelve minutes past 8 am
+get_time_name(8,0,AM)                # 8 o'clock am
+get_time_name(8,15,PM)               # a quarter after 8 pm
+get_time_name(8,30,AM)               # half past 8 am
+get_time_name(8,45,PM)               # a quater to 9 am
+get_time_name(8,12,AM)               # twelve minutes past 8 am
 '''
 
 # Minute constants
@@ -64,10 +68,10 @@ EIGHTTEEN = "eightteen"
 NINETEEN = "nineteen"
 
 # Get the English time name in a point in time
-def getTimeName(hours, minutes, period):
+def get_time_name(hours, minutes, period):
     
-    timeName = None
-    pastTheHour = ''
+    time_name = None
+    past_the_hour = ''
     
     if minutes == '45':
     
@@ -84,75 +88,75 @@ def getTimeName(hours, minutes, period):
         else:
             hours += 1
         
-        timeName = "a quarter to " + str(hours) + ' ' + period
+        time_name = "a quarter to " + str(hours) + ' ' + period
         
     elif minutes == '0':
-        timeName = str(hours) + " o'clock " + period
+        time_name = str(hours) + " o'clock " + period
     elif minutes == '15':
-        timeName = "a quarter after " + str(hours) + ' ' + period
+        time_name = "a quarter after " + str(hours) + ' ' + period
     elif minutes == '30':
-        timeName = "half past " + str(hours) + ' ' + period
+        time_name = "half past " + str(hours) + ' ' + period
     else:
-        pastTheHour = " minutes past " + str(hours) + ' ' + period
+        past_the_hour = " minutes past " + str(hours) + ' ' + period
         
         # Set time name for minutes between 1 and 19
-        if minutes == '1': timeName = ONE
-        if minutes == '2': timeName = TWO
-        if minutes == '3': timeName = THREE
-        if minutes == '4': timeName = FOUR
-        if minutes == '5': timeName = FIVE
-        if minutes == '6': timeName = SIX
-        if minutes == '7': timeName = SEVEN
-        if minutes == '8': timeName = EIGHT
-        if minutes == '9': timeName = NINE
-        if minutes == '10': timeName = TEN
-        if minutes == '11': timeName = ELEVEN
-        if minutes == '12': timeName = TWELVE
-        if minutes == '13': timeName = THIRTEEN
-        if minutes == '14': timeName = FOURTEEN
-        if minutes == '16': timeName = SIXTEEN
-        if minutes == '17': timeName = SEVENTEEN
-        if minutes == '18': timeName = EIGHTTEEN
-        if minutes == '19': timeName = NINETEEN       
+        if minutes == '1': time_name = ONE
+        if minutes == '2': time_name = TWO
+        if minutes == '3': time_name = THREE
+        if minutes == '4': time_name = FOUR
+        if minutes == '5': time_name = FIVE
+        if minutes == '6': time_name = SIX
+        if minutes == '7': time_name = SEVEN
+        if minutes == '8': time_name = EIGHT
+        if minutes == '9': time_name = NINE
+        if minutes == '10': time_name = TEN
+        if minutes == '11': time_name = ELEVEN
+        if minutes == '12': time_name = TWELVE
+        if minutes == '13': time_name = THIRTEEN
+        if minutes == '14': time_name = FOURTEEN
+        if minutes == '16': time_name = SIXTEEN
+        if minutes == '17': time_name = SEVENTEEN
+        if minutes == '18': time_name = EIGHTTEEN
+        if minutes == '19': time_name = NINETEEN       
         
         # Minutes name for twenties, thirties, forties, and fifties
         if minutes >= '20': 
-            timeName = getMinutesName(minutes)            
+            time_name = get_minutes_name(minutes)            
         
-    return timeName + pastTheHour
+    return time_name + past_the_hour
 
 # Get the English minute name
-def getMinutesName(minutes):   
-    if minutes[0] == '2': timeName = TWENTY
-    elif minutes[0] == '3': timeName = THIRTY
-    elif minutes[0] == '4': timeName = FORTY
-    else : timeName = FIFTY
+def get_minutes_name(minutes):   
+    if minutes[0] == '2': time_name = TWENTY
+    elif minutes[0] == '3': time_name = THIRTY
+    elif minutes[0] == '4': time_name = FORTY
+    else : time_name = FIFTY
 
-    if minutes[1] == '1': return timeName + '-' + ONE
-    if minutes[1] == '2': return timeName + '-' + TWO
-    if minutes[1] == '3': return timeName + '-' + THREE
-    if minutes[1] == '4': return timeName + '-' + FOUR
-    if minutes[1] == '5': return timeName + '-' + FIVE
-    if minutes[1] == '6': return timeName + '-' + SIX 
-    if minutes[1] == '7': return timeName + '-' + SEVEN
-    if minutes[1] == '8': return timeName + '-' + EIGHT
-    if minutes[1] == '9': return timeName + '-' + NINE 
+    if minutes[1] == '1': return time_name + '-' + ONE
+    if minutes[1] == '2': return time_name + '-' + TWO
+    if minutes[1] == '3': return time_name + '-' + THREE
+    if minutes[1] == '4': return time_name + '-' + FOUR
+    if minutes[1] == '5': return time_name + '-' + FIVE
+    if minutes[1] == '6': return time_name + '-' + SIX 
+    if minutes[1] == '7': return time_name + '-' + SEVEN
+    if minutes[1] == '8': return time_name + '-' + EIGHT
+    if minutes[1] == '9': return time_name + '-' + NINE 
     return ''
 
 # Read the hours, minutes, and period from user
-def getTime():
+def get_time():
     hours = int(input("Enter the hours (1-12):"))
     minutes = input("Enter minutes (0-59):")
     period= input("Enter period (AM/PM):").upper()
-    return hours,minutes,period    
+    return hours, minutes, period    
 
 # Main entry to program
 def main():
-    hours,minutes,period = getTime()
+    hours, minutes, period = get_time()
     if hours > 12 or hours < 0 or int(minutes) > 59 or int(minutes) < 0 or period != 'AM' and period != 'PM':
         print("*** Error:  Sorry, invalid time. Good-bye. ***")
     else:
-        timeName = getTimeName(hours,minutes,period)
-        print("The time is",timeName + '.')
+        time_name = get_time_name(hours, minutes, period)
+        print("The time is", time_name + '.')
         
 main()
