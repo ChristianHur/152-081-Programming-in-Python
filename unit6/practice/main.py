64f8032accdf6256e95ff36e3ca25374eb86f411
Christian Hur <chrisvhur@gmail.com>
2021-11-28 21:13:07 -0600

    Update main.py

--- unit6/practice/main.py
+++ unit6/practice/main.py
@@ -24,18 +24,20 @@ SOFTWARE.
 ------------
 Description:
 ------------
-A pet shop wants to give a discount to its clients if they buy one or more
-pets and at least five other items. The discount is equal to 20 percent of
-the cost of the other items, but not the pets. Implement a function
-def discount(prices, isPet, nItems)
+Write a program that reads 10 unique numbers and removes the smallest
+number from thet list. adds them to a list if they aren’t
 
-The function receives information about a particular sale. For the ith item,
-prices[i] is the price before any discount, and isPet[i] is true if the item
-is a pet.
+Part 1
+Write a function getInput() to read 10 unique numbers. When the list contains
+ten numbers, the program displays the contents.
 
-Write a program that prompts a cashier to enter each price and then a Y for a
-pet or N for another item. Use a price of –1 as a sentinel. Save the inputs
-in a list. Call the function that you implemented, and display the discount.
+Part 2
+Write a function removeMin() that removes the minimum value from a list
+without using the min function or remove method.
+
+Part 3
+Prompts the user to keep removing the next smallest from the list until
+they enter a sentinel value ('Q') to quit the program.
 '''
 import tennumbers as tn
 tn.main()
