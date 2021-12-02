f75fb433f144ce54c0807c4410b7b69bc51256fb
Christian Hur <chrisvhur@gmail.com>
2021-12-01 23:26:41 -0600

    Create dollars_1.py

---
+++ unit14/dollars_1.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+RECURSION (Recursive function) - a function that calls itself
+
+Write a program that calculates and displays the conversion of an
+entered number of dollars into currency denominations:
+100s, 50s, 20s, 10s, 5s, and 1s.
+
+Algorithm:  Using a counter (index)
+"""
+
+dollars = float(input("Enter amount:"))
+
+denominations = [100,50,20,10,5,1,.5,.25,.1,.05,.01]
+def count_denom( dollars, i ):
+    
+    if i == len(denominations): return  #DONE
+    
+    qty = int(dollars // denominations[i])
+    
+    if denominations[i] >= 1:
+        print(f'${str(denominations[i]).rjust(5)}: {qty}')
+    else:
+        print(f'{str( int(denominations[i]/.01)).rjust(5)}¢: {qty}')
+            
+    count_denom(dollars % denominations[i], i+1)
+
+
+if __name__ == "__main__":   
+  count_denom(dollars, 0)
